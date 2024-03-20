64acca88ce943912085c5b8e2ddf864c42d743a5
epdal <epdal@umich.edu>
2024-03-20 17:26:22 -0400

    changed the singleton pattern to immediately return if exists and have safer scene loads

--- Assets/Scripts/PathPlanning/GlobalUnitManager.cs
+++ Assets/Scripts/PathPlanning/GlobalUnitManager.cs
@@ -22,9 +22,11 @@ public class GlobalUnitManager : MonoBehaviour
         //there will be exactly one of these
         if(singleton == null){
             singleton = this;
+            DontDestroyOnLoad(gameObject);
         }
         else{
             Destroy(gameObject);
+            return;
         }
         
         
