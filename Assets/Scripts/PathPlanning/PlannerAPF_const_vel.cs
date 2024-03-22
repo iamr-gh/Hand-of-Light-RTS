d9146c9c984e084bd1b9b3c38cb7494e58f403ae
Ibrahim <roughtar@gmail.com>
2024-03-22 10:31:56 -0400

    moved some folders around to organize scenes

---
+++ Assets/Scripts/PathPlanning/PlannerAPF_const_vel.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+[RequireComponent(typeof(UnitAffiliation))]
+public class PlannerAPF_const_vel : Planner
+{
+    //eventually I would like to decouple from rb
+    //will need to put vel constPlannerAPF2Draints, make a util lib
+    public float goal_weight = 1;
+    public float obav_weight = 1;
+    public float friendly_av_weight = 1;
+    public float enemy_av_weight = 0;
+    //the degree which above is automated is kind of strange
+    public float maxvel;
+    public float obav_radius = 4;
+
+    private UnitAffiliation unitAffiliation;
+
+    protected override void Start()
+    {
+        base.Start();
+        TryGetComponent(out unitAffiliation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //movement in XZ
+        Vector2 pos_2d = new Vector2(transform.position.x, transform.position.z);
+        var goal_offset = goal - pos_2d;
+        //more components of APF
+
+        //obav, inv square of distance
+        var obav_offset = Vector2.zero;
+        var friendly_av_offset = Vector2.zero;
+        var enemy_av_offset = Vector2.zero;
+
+        //ignoring any enemy av weight
+
+        foreach (GameObject obj in GlobalUnitManager.singleton.FindNearby(transform.position, obav_radius))
+        {
+            if(obj == gameObject){
+                continue;
+            }
+            var offset = transform.position - obj.transform.position;
+            var offset2d = new Vector2(offset.x, offset.z); //this transform is annoying
+            if (TryGetComponent(out UnitAffiliation unitaff))
+            {
+                //ignore unts in this rn
+                if (unitaff.affiliation == unitAffiliation.affiliation)
+                {
+                    friendly_av_offset += (1 / offset2d.sqrMagnitude) * offset2d.normalized;
+                }
+                else
+                {
+                    enemy_av_offset += (1 / offset2d.sqrMagnitude) * offset2d.normalized;
+                }
+            }
+            else{
+                obav_offset += (1 / offset2d.sqrMagnitude) * offset2d.normalized;
+            }
+
+
+        }
+
+
+
+        //weight different priorities
+        var overall = goal_weight * goal_offset;
+        overall += obav_weight * obav_offset;
+        overall += friendly_av_weight * friendly_av_offset;
+        overall += enemy_av_weight * enemy_av_offset;
+
+        Vector3 next_vel = new Vector3(overall.x, 0, overall.y);
+        //instead, have a fixed magnitude on planner
+        next_vel = next_vel.normalized*maxvel;
+
+        //note that AI now needs to be involved
+        rb.velocity = next_vel;
+    }
+
+}
