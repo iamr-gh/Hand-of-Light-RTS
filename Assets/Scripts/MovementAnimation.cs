9b9f52a2e5390115978c563ba2fc8a2117ac0dcb
epdal <epdal@umich.edu>
2024-03-31 00:07:07 -0400

    rotation works well, except for jittering caused by planner

--- Assets/Scripts/MovementAnimation.cs
+++ Assets/Scripts/MovementAnimation.cs
@@ -6,34 +6,44 @@ using UnityEngine;
 
 public class MovementAnimation : MonoBehaviour
 {
-    //private SpriteRenderer spriteRenderer;
     private Rigidbody rb;
     private string facing = "Left";
     private bool isTurning = false;
     [SerializeField] float turnDuration = 0.2f;
+    [SerializeField] float hopHeight = 0.2f;
+    [SerializeField] float hopRotation = 5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        //spriteRenderer = GetComponent<SpriteRenderer>();
         rb = GetComponentInParent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(isTurning) { return; } // Only start turning again after finishing once
+        // TODO: Have character hop
+        //if(rb.velocity.magnitude != 0)
+        //{
+        //    
+        //    transform.position = new Vector3(transform.position.x,
+        //                                     hopHeight * Mathf.Abs(Mathf.Sin(Time.time % (2 * Mathf.PI))),
+        //                                     transform.position.z);
+        //    transform.rotation = Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y,
+        //                                          hopRotation * Mathf.Sin(Time.time % (2 * Mathf.PI)));
+        //}
+        if (isTurning) { return; } // Only start turning again after finishing once
 
-        Debug.Log(facing + " | " + rb.velocity);
-        Quaternion targetRotation = Quaternion.Euler(transform.eulerAngles.x * -1, transform.eulerAngles.y + 180, transform.eulerAngles.z);
         if (facing == "Left" && rb.velocity.x > 0)
         {
             facing = "Right";
+            Quaternion targetRotation = Quaternion.Euler(-30, 180, 0);
             StartCoroutine(RotateObject(transform.rotation, targetRotation, turnDuration));
         }
         else if(facing == "Right" && rb.velocity.x < 0)
         {
             facing = "Left";
+            Quaternion targetRotation = Quaternion.Euler(30, 0, 0);
             StartCoroutine(RotateObject(transform.rotation, targetRotation, turnDuration));
         }
     }
