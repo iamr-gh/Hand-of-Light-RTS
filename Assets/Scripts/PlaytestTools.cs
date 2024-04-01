ef851e320f9480dfd6cf2607b1235953357cc7b7
Ibrahim <roughtar@gmail.com>
2024-03-31 22:32:52 -0400

    added playtest tools

---
+++ Assets/Scripts/PlaytestTools.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+//R to reload the level, G to reset the whole game, 
+//number keys to reset to particular level?
+public class PlaytestTools : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.R)){
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+        
+        if(Input.GetKeyDown(KeyCode.G)){
+            SceneManager.LoadScene(0);
+        }
+    }
+}
