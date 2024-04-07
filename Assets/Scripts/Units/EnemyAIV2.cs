f46c352d109e790eb5b364e4761cc6e5d742011c
Ibrahim <roughtar@gmail.com>
2024-04-07 00:33:36 -0400

    made a social AI that works well enough

--- Assets/Scripts/Units/EnemyAIV2.cs
+++ Assets/Scripts/Units/EnemyAIV2.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SearchService;
 using UnityEngine;
 
 public class EnemyAIV2 : UnitAIV2
 {
+    //centroid method is bad, let's just let them communicate target
     public float social_radius = 3.0f; //neighbors you can see
-    public float change_tolerance = 0.5f;
-    Vector3 last_nearby_centroid;
+    public GameObject seenTarget = null;
 
     //all the fancy logic from user controlled agents(probably less nessecary)
     protected override void Start()
@@ -14,7 +15,7 @@ public class EnemyAIV2 : UnitAIV2
         base.Start();
         // StopAllCoroutines();
         // StartCoroutine(base.WeaponSystemManager());
-        last_nearby_centroid = calcCentroid();
+        // (last_nearby_centroid, last_num) = calcCentroid();
         //NO ANTI JITTER CHECKER
         // stopped = false;
     }
@@ -22,39 +23,44 @@ public class EnemyAIV2 : UnitAIV2
     protected override void Update()
     {
         base.Update();
-        //if not attacking, and neighbors move, follow them a bit
+
+        if (target != null)
+        {
+            seenTarget = target;
+        }
+
+        //if not attacking, and neighbors have a target, follow them
         if (target == null && attacking)
         {
-            var next_centroid = calcCentroid();
-            var change = next_centroid - last_nearby_centroid;
-            if (change.magnitude > change_tolerance)
+            var (centroid, has_target) = calcCentroid();
+            if (has_target)
             {
-                // last_nearby_centroid = next_centroid;
-                // this can create a weird momentum
-                AttackMoveToCoordinate(transform.position + change);
-                last_nearby_centroid = next_centroid;
+                AttackMoveToCoordinate(centroid);
             }
         }
 
     }
 
-    private Vector3 calcCentroid()
+    private (Vector3, bool) calcCentroid()
     {
         var neighbors = GlobalUnitManager.singleton.FindNearby(transform.position, social_radius);
         // get centroid of neighbors
         Vector3 centroid = Vector3.zero;
 
-        //purposefully include self
+        bool seen = false;
         foreach (GameObject obj in neighbors)
         {
-            if(obj != null){
+            if (obj == gameObject || obj.TryGetComponent(out EnemyAIV2 enemyAIV2) && enemyAIV2.seenTarget != null)
+            {
+                //could give a target to each other if we want, for more graph effects. This will be good enough for now tho
+                if (obj != gameObject)
+                    seen = true;
                 centroid += obj.transform.position;
             }
         }
 
         centroid /= neighbors.Count;
-        return centroid;
-
+        return (centroid, seen);
     }
 
 }
