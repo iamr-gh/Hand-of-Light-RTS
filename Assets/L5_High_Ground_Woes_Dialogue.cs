13f1c31fd9ac66868ab7bd4b37cb1c49f4f58260
Ibrahim <roughtar@gmail.com>
2024-04-14 21:57:42 -0400

    l5 is done

---
+++ Assets/L5_High_Ground_Woes_Dialogue.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class L5_High_Ground_Woes_Dialogue : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       StartCoroutine(dialogue()); 
+    }
+    
+    IEnumerator dialogue(){
+        var input = GlobalUnitManager.singleton.GetComponent<PlayerInput>();
+        var cam_move = Camera.main.GetComponent<cameraMovement>();
+        var controlSystem = GlobalUnitManager.singleton.GetComponent<ControlSystem>();
+
+        //disable controls for dialogue
+        input.actions.FindActionMap("Player").Disable();
+        cam_move.enabled = false;
+        
+        
+        ToastSystem.instance.SendDialogue("We have forces on the other side of this pass that need reinforcements!",autoDismissTime: 5f);
+        // yield return new WaitForSeconds(5f);
+        // ToastSystem.instance.SendDialogue("Cross through this pass ASAP!",autoDismissTime: 3f);
+        // yield return new WaitForSeconds(3f);
+
+        ToastSystem.instance.SendDialogue("General, there are archers on the cliffs...",
+        portrait: GlobalUnitManager.singleton.GetPortrait("Melee").Item1, autoDismissTime: 5f);
+        // yield return new WaitForSeconds(5f);
+        
+        ToastSystem.instance.SendDialogue("Let's use these new mages, they should be able to blind them long enough for you to run past.",autoDismissTime: 5f);
+        // yield return new WaitForSeconds(5f);
+        
+        ToastSystem.instance.SendDialogue("Reporting in, ready to blind some archers.",
+        portrait: GlobalUnitManager.singleton.GetPortrait("Mage").Item1, autoDismissTime: 5f); yield return new WaitForSeconds(5f);
+        
+        input.actions.FindActionMap("Player").Enable();
+        cam_move.enabled = true;
+
+        yield return new WaitForSeconds(15f);
+
+        var notif6 = ToastSystem.instance.SendNotification("Press 1 to use a selected unit's ability.", false);
+        yield return new WaitForSeconds(4f);
+        ToastSystem.instance.DismissNotification(notif6);
+    }
+
+}
