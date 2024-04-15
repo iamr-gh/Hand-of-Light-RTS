efd764e5bb20e5927ccc5243bb9a743d4c9138ca
epdal <epdal@umich.edu>
2024-04-15 02:02:33 -0400

    changes to level

--- Assets/CommandoExpo.cs
+++ Assets/CommandoExpo.cs
@@ -5,20 +5,23 @@ using UnityEngine;
 public class CommandoExpo : MonoBehaviour
 {
     public GameObject commando;
+    bool triggered = false;
 
     private void OnTriggerEnter(Collider other) {
         UnitAffiliation affiliation = other.gameObject.GetComponent<UnitAffiliation>();
-        if (affiliation != null && affiliation.affiliation == "White") {
+        if (!triggered && affiliation != null && affiliation.affiliation == "White") {
             StartCoroutine(SeeCommando());
+            triggered = true;
         }
     }
 
     IEnumerator SeeCommando() {
         commando.SetActive(true);
         Vector3 cameraStartPosition = Camera.main.transform.position;
-        yield return StartCoroutine(ScrollCamera(cameraStartPosition, commando.transform.position, 0.5f));
+        Vector3 newCameraPosition = new Vector3(commando.transform.position.x, cameraStartPosition.y, commando.transform.position.z - 20f);
+        //yield return StartCoroutine(ScrollCamera(cameraStartPosition, newCameraPosition, 1f));
         yield return new WaitForSeconds(0.5f);
-        yield return StartCoroutine(ScrollCamera(Camera.main.transform.position, cameraStartPosition, 0.5f));
+        //yield return StartCoroutine(ScrollCamera(Camera.main.transform.position, cameraStartPosition, 1f));
     }
 
     IEnumerator ScrollCamera(Vector3 initialPosition, Vector3 finalPosition, float duration_sec) {
