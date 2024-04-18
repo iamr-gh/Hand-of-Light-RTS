b620d854abf5a3913ef7d22d853458e27911ac85
Ibrahim <roughtar@gmail.com>
2024-04-18 19:42:18 -0400

    made tutorial D more substantive, still need to add some mage system

--- Assets/Tutorial4Sequence.cs
+++ Assets/Tutorial4Sequence.cs
@@ -3,16 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
-public class Tutorial4Sequence : MonoBehaviour
-{
+public class Tutorial4Sequence : MonoBehaviour {
+    public GameObject enemyToKill;
     // Start is called before the first frame update
-    void Start()
-    {
-        StartCoroutine(tutorial());    
+    
+    bool dialogueDismissed = false;
+    
+    void Start() {
+        ToastSystem.instance.onDialogueAdvanced.AddListener(dismissTracker);
+        StartCoroutine(tutorial());
     }
+    
+    void dismissTracker() {
+        //could be switched to a counter at some point, but I think this is easier
+        dialogueDismissed = true;
+    }
+    
+    
 
-    IEnumerator tutorial()
-    {
+    IEnumerator tutorial() {
         yield return null;
         var input = GlobalUnitManager.singleton.GetComponent<PlayerInput>();
         var cam_move = Camera.main.GetComponent<cameraMovement>();
@@ -21,72 +30,121 @@ public class Tutorial4Sequence : MonoBehaviour
         input.actions.FindActionMap("Player").Disable();
         cam_move.enabled = false;
 
+        dialogueDismissed = false;
         ToastSystem.instance.SendDialogue("Some special units have abilities, which will appear in an additional menu", autoDismissTime: 5f);
-        yield return new WaitForSeconds(5f);
+        while(!dialogueDismissed) {
+            yield return null;
+        }
 
         var notif1 = ToastSystem.instance.SendNotification("Select the Commando at the top of the screen", autoDismiss: false);
         input.actions["Select"].Enable();
-        
-        while(true){
+
+        while (true) {
             yield return null;
             bool found = false;
-            foreach (var unit in controlSystem.controlledUnits)
-            {
-                if(unit.TryGetComponent(out Ability ability)){
+            foreach (var unit in controlSystem.controlledUnits) {
+                if (unit.TryGetComponent(out Ability ability)) {
                     found = true;
                     break;
                 }
             }
-            if (found){
+            if (found) {
                 break;
             }
         }
 
         ToastSystem.instance.DismissNotification(notif1);
-        
+
         input.actions["Ability 1"].Enable();
 
 
         // ToastSystem.instance.SendDialogue("Press 1 and click to use the Commando's ability to recall your units", autoDismiss: false);
 
         var notif6 = ToastSystem.instance.SendNotification("Press 1 and click your units to use Commando's recall!", false);
-        
+
         //get positions of existing non-commando units
         Dictionary<GameObject, Vector3> unitPositions = new();
-        foreach(var unit in GlobalUnitManager.singleton.allManaged){
-            if(!unit.TryGetComponent(out Ability ability)){
-                unitPositions[unit] = unit.transform.position;
+        foreach (var unit in GlobalUnitManager.singleton.allManaged) {
+            if (!unit.TryGetComponent(out Ability ability)) {
+                //hard coded
+                if (unit.TryGetComponent(out UnitAffiliation affiliation) && affiliation.affiliation == "White") {
+                    unitPositions[unit] = unit.transform.position;
+                }
             }
         }
 
-        while (true)
-        {
+        while (true) {
             yield return null;
             bool allRecalled = true;
-            foreach (var unit in GlobalUnitManager.singleton.allManaged)
-            {
-                if (!unit.TryGetComponent(out Ability ability))
-                {
-                    if (unit.transform.position == unitPositions[unit])
-                    {
-                        allRecalled = false;
-                        break;
+            foreach (var unit in GlobalUnitManager.singleton.allManaged) {
+                if (!unit.TryGetComponent(out Ability ability)) {
+                    if (unit.TryGetComponent(out UnitAffiliation affiliation) && affiliation.affiliation == "White") {
+                        if (unit.transform.position == unitPositions[unit]) {
+                            allRecalled = false;
+                            break;
+                        }
                     }
                 }
             }
-            if (allRecalled)
-            {
+            if (allRecalled) {
                 break;
             }
         }
         ToastSystem.instance.DismissNotification(notif6);
 
         ToastSystem.instance.AdvanceDialogue();
-        input.actions.FindActionMap("Player").Enable();
 
+        input.actions.FindActionMap("Player").Enable();
         cam_move.enabled = true;
 
-        ToastSystem.instance.SendDialogue("Tutorial Complete!", autoDismiss: false);
+        dialogueDismissed = false;
+        ToastSystem.instance.SendDialogue("Recall can be used to get your units out of sticky situations.", autoDismissTime: 3.0f);
+        while(!dialogueDismissed) {
+            yield return null;
+        }
+
+        //kill enemy unit
+        ToastSystem.instance.SendDialogue("It also works on enemy units, use it to engage the one on the plateau", autoDismiss: false);
+
+
+        while(true) {
+            yield return null;
+            //wait till death
+            if (enemyToKill == null) {
+                break;
+            }
+        }
+        
+        ToastSystem.instance.AdvanceDialogue();
+
+        //wait till death of this unit
+        dialogueDismissed = false;
+        ToastSystem.instance.SendDialogue("As seen, this is a double edged sword.", autoDismiss: false);
+
+        while(!dialogueDismissed) {
+            yield return null;
+        }
+
+        //honestly just make another tutorial
+
+        //maybe some camera pan
+        // ToastSystem.instance.SendDialogue("There are other units with abilities", autoDismiss: false);
+        // ToastSystem.instance.SendDialogue("These mages can blind archers, to allow your units to safely pass by", autoDismiss: false);
+        
+        //come up with a way to introduce mage
+        
+        
+
+
+
+
+        //how I transition to mages?       
+        // will be another tutorial??
+
+        // should this have a portrait?
+        ToastSystem.instance.SendDialogue("Promotion Exam Complete! Let's get you into the field.", autoDismiss: false);
+
+        yield return new WaitForSeconds(5f);
 
         //increment to next scene's build index
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
