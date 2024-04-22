b308f7b23a580a5f761b3021bbfca3d54c91f24b
Ibrahim <roughtar@gmail.com>
2024-04-22 01:56:01 -0400

    pushing l2 and l9 changes

---
+++ Assets/ClearEnemiesObjective.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClearEnemiesObjective : MonoBehaviour
+{
+    private float InitialEnemyCount;
+    private float currentEnemyCount;
+    public ulong currentObj = 0;
+
+    void Start()
+    {
+       InitialEnemyCount = nestedChildCount(transform); 
+       currentEnemyCount = InitialEnemyCount;
+       currentObj = ToastSystem.instance.SendObjective("Clear all enemies " + (InitialEnemyCount - nestedChildCount(transform)) + "/" + InitialEnemyCount);
+    }
+    
+    private int nestedChildCount(Transform parent){
+        if(parent.childCount == 0){
+            return 1;
+        }
+        int count = 0;
+        foreach(Transform child in parent){
+            //just doing on level, instead of full recursion
+            count += child.childCount;
+        }
+        return count;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(currentEnemyCount != nestedChildCount(transform)){
+            ToastSystem.instance.RemoveObjective(currentObj);
+            currentObj = ToastSystem.instance.SendObjective("Clear all enemies " + (InitialEnemyCount - nestedChildCount(transform)) + "/" + InitialEnemyCount);
+        }
+       currentEnemyCount = nestedChildCount(transform);
+    }
+}
--- Assets/Scripts/Units/EnemyAIV2.cs
+++ Assets/Scripts/Units/EnemyAIV2.cs
@@ -2,15 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyAIV2 : UnitAIV2
-{
+public class EnemyAIV2 : UnitAIV2 {
     //centroid method is bad, let's just let them communicate target
     public float social_radius = 3.0f; //neighbors you can see
     public GameObject seenTarget = null;
 
     //all the fancy logic from user controlled agents(probably less nessecary)
-    protected override void Start()
-    {
+    protected override void Start() {
         base.Start();
         // StopAllCoroutines();
         // StartCoroutine(base.WeaponSystemManager());
@@ -19,46 +17,53 @@ public class EnemyAIV2 : UnitAIV2
         // stopped = false;
     }
 
-    protected override void Update()
-    {
+    protected override void Update() {
         base.Update();
 
-        if (target != null)
-        {
+        if (target != null) {
             seenTarget = target;
         }
 
         //if not attacking, and neighbors have a target, follow them
-        if (target == null && attacking)
-        {
+        if (target == null && attacking) {
             var (centroid, has_target) = calcCentroid();
-            if (has_target && (transform.position - navAgent.destination).magnitude <= goal_leash_a_move)
-            {
-                AttackMoveToCoordinate(centroid);
+            // seenTarget = has_target ? seenTarget : null;
+            if (has_target && (transform.position - navAgent.destination).magnitude <= goal_leash_a_move) {
+                //this is being set to 0 for some reason
+                //terrible patch fix, but will be fine for now I guess
+                //OH it's going close to zero too?
+                if (centroid != Vector3.zero) {
+                    AttackMoveToCoordinate(centroid);
+                }
             }
         }
 
     }
 
-    private (Vector3, bool) calcCentroid()
-    {
+    private (Vector3, bool) calcCentroid() {
         var neighbors = GlobalUnitManager.singleton.FindNearby(transform.position, social_radius);
         // get centroid of neighbors
         Vector3 centroid = Vector3.zero;
+        var neigh_count = 0;
 
         bool seen = false;
-        foreach (GameObject obj in neighbors)
-        {
-            if (obj == gameObject || obj.TryGetComponent(out EnemyAIV2 enemyAIV2) && enemyAIV2.seenTarget != null)
-            {
+        foreach (GameObject obj in neighbors) {
+            if (obj == gameObject || obj.TryGetComponent(out EnemyAIV2 enemyAIV2) && enemyAIV2.seenTarget != null) {
                 //could give a target to each other if we want, for more graph effects. This will be good enough for now tho
-                if (obj != gameObject)
+                if (obj != gameObject){
                     seen = true;
-                centroid += obj.transform.position;
+                    centroid += obj.transform.position;
+                    neigh_count++;
+                }
             }
         }
 
-        centroid /= neighbors.Count;
+        centroid /= neigh_count;
+
+        //this really shouldn't be possible
+        if (centroid == Vector3.zero) {
+            centroid = transform.position;
+        }
         return (centroid, seen);
     }
 
