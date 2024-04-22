0528d285564a7b7b4855406dc7a9b71f012f4116
Brian Song <brsong@umich.edu>
2024-04-22 02:46:33 -0400

    finished regular level dialogue

--- Assets/FinalBattle.cs
+++ Assets/FinalBattle.cs
@@ -24,30 +24,39 @@ public class FinalBattle : MonoBehaviour
 
         input.actions.FindActionMap("Player").Disable();
         cam_move.enabled = false;
-        yield return new WaitForSeconds(0.1f);
-        ToastSystem.instance.SendDialogue("Is that...?", autoDismiss: false);
-        while (!Input.GetKeyDown(KeyCode.Space)) {
-            yield return null;
-        }
-        ToastSystem.instance.SendDialogue("Quickly! Back into position!", autoDismiss: false);
-        while (!Input.GetKeyDown(KeyCode.Space)) {
-            yield return null;
-        }
-        ToastSystem.instance.SendDialogue("But sir! What about the Seleneians?", portraitLabel: "Knight", portrait: GlobalUnitManager.singleton.GetPortrait("Melee").Item1, autoDismiss: false);
-        while (!Input.GetKeyDown(KeyCode.Space)) {
-            yield return null;
-        }
-        ToastSystem.instance.SendDialogue("Get into position with them! The incoming Astran force is too large for us to fight alone!", autoDismiss: false);
-        while (!Input.GetKeyDown(KeyCode.Space)) {
-            yield return null;
-        }
-        ToastSystem.instance.SendDialogue("Move move move!", autoDismiss: false);
-        while (!Input.GetKeyDown(KeyCode.Space)) {
-            yield return null;
-        }
-        ToastSystem.instance.SendDialogue("Sir yes sir!", portraitLabel: "Knight", portrait: GlobalUnitManager.singleton.GetPortrait("Melee").Item1, autoDismiss: false);
-        input.actions.FindActionMap("Player").Enable();
-        cam_move.enabled = true;
+        //yield return new WaitForSeconds(0.1f);
+        //ToastSystem.instance.SendDialogue("Is that...?", autoDismiss: false);
+        //while (!Input.GetKeyDown(KeyCode.Space)) {
+        //    yield return null;
+        //}
+        //ToastSystem.instance.SendDialogue("Quickly! Back into position!", autoDismiss: false);
+        //while (!Input.GetKeyDown(KeyCode.Space)) {
+        //    yield return null;
+        //}
+        //ToastSystem.instance.SendDialogue("But sir! What about the Seleneians?", portraitLabel: "Knight", portrait: GlobalUnitManager.singleton.GetPortrait("Melee").Item1, autoDismiss: false);
+        //while (!Input.GetKeyDown(KeyCode.Space)) {
+        //    yield return null;
+        //}
+        //ToastSystem.instance.SendDialogue("Get into position with them! The incoming Astran force is too large for us to fight alone!", autoDismiss: false);
+        //while (!Input.GetKeyDown(KeyCode.Space)) {
+        //    yield return null;
+        //}
+        //ToastSystem.instance.SendDialogue("Move move move!", autoDismiss: false);
+        //while (!Input.GetKeyDown(KeyCode.Space)) {
+        //    yield return null;
+        //}
+        //ToastSystem.instance.SendDialogue("Sir yes sir!", portraitLabel: "Knight", portrait: GlobalUnitManager.singleton.GetPortrait("Melee").Item1, autoDismiss: false);
+        //input.actions.FindActionMap("Player").Enable();
+        //cam_move.enabled = true;
+
+        ToastSystem.instance.SendDialogue("That’s a lot of enemy troops.",
+        portraitLabel: "General", autoDismissTime: 5f, audioClip: Resources.Load<AudioClip>("Audio/general lines/General_18"));
+
+        ToastSystem.instance.SendDialogue("Alright! Let’s utilize every tactic we’ve employed to make it this far! Let’s crush them and go home!",
+        portraitLabel: "General", autoDismissTime: 5f);
+
+        ToastSystem.instance.SendDialogue("Yes sir!",
+        portraitLabel: "Knight", portrait: GlobalUnitManager.singleton.GetPortrait("Melee").Item1, autoDismissTime: 6f, audioClip: Resources.Load<AudioClip>("Audio/Scout Lines/ScoutLine15"));
 
         //give them some time to look at what they have
         yield return new WaitForSeconds(7f);
---
+++ Assets/FinalBattleEnd.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinalBattleScript : MonoBehaviour {
+    public GameObject goal;
+    // Start is called before the first frame update
+    void Start() {
+
+        StartCoroutine(WaitForEnemiesDefeated());
+    }
+
+    int GetActiveChildCount() {
+        int activeChildren = 0;
+        for (int idx = 0; idx < transform.childCount; idx++) {
+            GameObject child = transform.GetChild(idx).gameObject;
+            if (child.activeSelf == true) {
+                activeChildren++;
+            }
+        }
+
+        return activeChildren;
+    }
+
+    IEnumerator WaitForEnemiesDefeated() {
+        // Wait for all enemies to be defeated
+        while (true) {
+            if (GetActiveChildCount() <= 0) { break; }
+            yield return null;
+        }
+        StartCoroutine(win());
+
+        goal.GetComponent<GoalScript>().SetAllEnemiesDefeated(true);
+    }
+    IEnumerator win() {
+        ToastSystem.instance.AdvanceDialogue();
+
+        ToastSystem.instance.SendDialogue("We did it!",
+        portraitLabel: "Knight", portrait: GlobalUnitManager.singleton.GetPortrait("Melee").Item1, autoDismissTime: 6f, audioClip: Resources.Load<AudioClip>("Audio/Scout Lines/ScoutLine16"));
+
+        ToastSystem.instance.SendDialogue("Victory is ours!",
+        portraitLabel: "General", autoDismissTime: 5f, audioClip: Resources.Load<AudioClip>("Audio/general lines/General_19"));
+
+        yield return new WaitForSeconds(3f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
