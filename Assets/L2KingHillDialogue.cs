ec31463752aebbc94b8f417594d7a5db8045b355
Brian Song <brsong@umich.edu>
2024-04-22 00:42:54 -0400

    added general's lines

--- Assets/L2KingHillDialogue.cs
+++ Assets/L2KingHillDialogue.cs
@@ -26,7 +26,7 @@ public class L2KingHillDialogue : MonoBehaviour
 
 
         ToastSystem.instance.SendDialogue("General. We've located an enemy encampment.",
-        portraitLabel: "Knight", portrait: GlobalUnitManager.singleton.GetPortrait("Melee").Item1, autoDismissTime: 6f);
+        portraitLabel: "Knight", portrait: GlobalUnitManager.singleton.GetPortrait("Melee").Item1, autoDismissTime: 6f, audioClip: Resources.Load<AudioClip>("Audio/Scout Lines/ScoutLine6"));
 
         ToastSystem.instance.SendDialogue("We believe this is where their forces have been moving through to avoid detection.",
         portraitLabel: "Knight", portrait: GlobalUnitManager.singleton.GetPortrait("Melee").Item1, autoDismissTime: 6f);
@@ -34,18 +34,18 @@ public class L2KingHillDialogue : MonoBehaviour
         //make general speaker
         ToastSystem.instance.SendDialogue("It's a good tactical location, but they've chosen a poor position for a fight.",
         //using ranged for general rn
-        portraitLabel: "Archer", portrait: GlobalUnitManager.singleton.GetPortrait("Ranged").Item1, autoDismissTime: 4f);
+        portraitLabel: "General", autoDismissTime: 4f);
 
 
         //make general speaker
         ToastSystem.instance.SendDialogue("Archers, sieze the high ground and snipe them from afar.",
         //using ranged for general rn
-        portraitLabel: "Archer", portrait: GlobalUnitManager.singleton.GetPortrait("Ranged").Item1, autoDismissTime: 5f);
+        portraitLabel: "General", autoDismissTime: 5f);
 
 
 
         ToastSystem.instance.SendDialogue("Yes sir.",
-        portraitLabel: "Knight", portrait: GlobalUnitManager.singleton.GetPortrait("Melee").Item1, autoDismissTime: 2f);
+        portraitLabel: "Knight", portrait: GlobalUnitManager.singleton.GetPortrait("Melee").Item1, autoDismissTime: 2f, audioClip: Resources.Load<AudioClip>("Audio/Scout Lines/ScoutLine7"));
 
         yield return new WaitForSeconds(5f);
 
@@ -60,23 +60,26 @@ public class L2KingHillDialogue : MonoBehaviour
         //on win
 
         ToastSystem.instance.SendDialogue("Enemy base has been cleared, sir.",
-        portraitLabel: "Knight", portrait: GlobalUnitManager.singleton.GetPortrait("Melee").Item1, autoDismissTime: 5f);
+        portraitLabel: "Knight", portrait: GlobalUnitManager.singleton.GetPortrait("Melee").Item1, autoDismissTime: 5f, audioClip: Resources.Load<AudioClip>("Audio/Scout Lines/ScoutLine8"));
 
         yield return new WaitForSeconds(5f);
 
         ToastSystem.instance.SendDialogue("We need to move fast. The unit here has likely already contacted the nearest base. \r\n",
-        portraitLabel: "Archer", portrait: GlobalUnitManager.singleton.GetPortrait("Ranged").Item1, autoDismissTime: 5f);
+        portraitLabel: "General", autoDismissTime: 5f);
 
         yield return new WaitForSeconds(5f);
 
-        ToastSystem.instance.SendDialogue("We need to blitz through enemy territory and take that base out before the enemy can regroup and launch a counterattack. Call for a commando and speeder unit.",
-        portraitLabel: "Archer", portrait: GlobalUnitManager.singleton.GetPortrait("Ranged").Item1, autoDismissTime: 7f);
+        ToastSystem.instance.SendDialogue("We need to blitz through enemy territory and take that base out before the enemy can regroup and launch a counterattack.",
+        portraitLabel: "General", autoDismissTime: 7f);
+
+        ToastSystem.instance.SendDialogue("Call for a commando and speeder unit.",
+        portraitLabel: "General", autoDismissTime: 7f);
 
         yield return new WaitForSeconds(7f);
 
         //level complete, move onto the next one
         ToastSystem.instance.SendDialogue("Understood.",
-        portraitLabel: "Knight", portrait: GlobalUnitManager.singleton.GetPortrait("Melee").Item1, autoDismissTime: 2f);
+        portraitLabel: "Knight", portrait: GlobalUnitManager.singleton.GetPortrait("Melee").Item1, autoDismissTime: 2f, audioClip: Resources.Load<AudioClip>("Audio/Scout Lines/ScoutLine9"));
 
         yield return new WaitForSeconds(2f);
 
